0e3bf49c86b1f395c39422f77e15cc635babb5e4
Benedikt Willi <ben.willi@gmail.com>
2022-08-16 08:18:53 +0200

    Update inlinepatterns.py

--- mdx_oembed/inlinepatterns.py
+++ mdx_oembed/inlinepatterns.py
@@ -8,7 +8,7 @@ LOG = logging.getLogger(__name__)
 
 
 OEMBED_LINK_RE = r'\!\[([^\]]*)\]\(((?:https?:)?//[^\)]*)' \
-                 r'(?<!png)(?<!jpg)(?<!jpeg)(?<!gif)\)'
+                 r'(?<!png)(?<!jpg)(?<!jpeg)(?<!gif)(?<!avif)(?<!webp)\)'
 
 
 class OEmbedLinkPattern(Pattern):
@@ -19,17 +19,15 @@ class OEmbedLinkPattern(Pattern):
 
     def handleMatch(self, match):
         html = self.get_oembed_html_for_match(match)
-        LOG.warn(html)
         if html is None:
             return None
         else:
             html = "<figure class=\"oembed\">%s</figure>" % html
-            placeholder = self.markdown.htmlStash.store(html)
+            placeholder = self.md.htmlStash.store(html)
             return placeholder
 
     def get_oembed_html_for_match(self, match):
         url = match.group(3).strip()
-        LOG.warn(url)
         try:
             response = self.consumer.embed(url)
         except oembed.OEmbedNoEndpoint:
